b7a0036a22367887b0cb3b6752a60b72765e8d84
Ikaia Melton <91578371+ikaia@users.noreply.github.com>
2023-07-13 10:44:12 -0400

    Add files via upload

---
+++ A1p2.java
@@ -0,0 +1,13 @@
+
+public class A1p2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int i = 33; i <= 126; i++) {
+			
+			System.out.println((char) i +"\tASCII value:"+i);
+		}
+	}
+
+}
